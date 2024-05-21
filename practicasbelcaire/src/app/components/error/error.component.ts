f3690634992100ee7788c54a71a0d312e213a436
feitan173 <juanxillo69@gmail.com>
2024-05-21 09:31:12 +0200

    migracion a scss

--- practicasbelcaire/src/app/components/error/error.component.ts
+++ practicasbelcaire/src/app/components/error/error.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrl: './error.component.css'
+  styleUrl: './error.component.scss'
 })
 export class ErrorComponent {
 
